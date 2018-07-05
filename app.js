145b95c175859f08bb386391d0487ee944fbba49
Paulo Bressan <paulo.bressan>
2018-07-05 16:33:18 -0300

    home

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 //Importando as configurações servidor e invocando a, o modulo retorna uma função.
 var app = require('./config/express')();
+
 //importando as rotas de produtos
-var rotasprodutos = require('./app/routes/produtos')(app);
+require('./app/routes/produtos')(app);
+
+//importando rota da home
+require('./app/routes/home')(app);
 
 //configurando a porta que o servidor vai escutar.
 app.listen(3000, ()=>{
