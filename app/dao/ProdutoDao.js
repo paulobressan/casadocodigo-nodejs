9ce0cbfe4e38c4a7c996be453cb846e0f4048f13
Paulo Bressan <paulo.bressan>
2018-07-26 14:14:01 -0300

    Cadastrando produtos, retornando json

--- app/dao/ProdutoDao.js
+++ app/dao/ProdutoDao.js
@@ -8,4 +8,10 @@ module.exports = () => class ProdutoDao{
     lista(callback){
         this._connection.query('select * from livros', callback);
     }
+
+    salva(produto, callback){
+        //o driver do mysql aceita uma sintaxe um pouco diferente onde vamos conseguir setar o que vamos inserir
+        //e colocar ? como parametro que é um obj, logo em seguida o parametro 
+        this._connection.query(`insert into livros set ?`, produto, callback);
+    }
 }
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -4,41 +4,49 @@ module.exports = (app) => {
     //atendendo requisições e enviando respostas
     app.get('/produtos', (request, response) => {
         console.log("Listando");
-        
+
         //Criando uma instancia da classe ConnectionFactory buscando-a com o express-load
         //e logo em seguida executando o metodo que cria uma conexão.
-        var connection = new app.infra.ConnectionFactory().createConnection();        
+        var connection = new app.infra.ConnectionFactory().createConnection();
 
         //separando a responsabilidade do banco para uma classe DAO.
         //instanciando a classe e buscando-a pelo express-load
         var produtoDao = new app.dao.ProdutoDao(connection);
 
         produtoDao.lista((err, results) => {
-            //retornando um json para o navegador.
-            //response.send(results);
-            response.render("produtos/lista", {lista: results});
+            response.format({
+                html: () => {
+                    //retornando um json para o navegador.
+                    //response.send(results);
+                    response.render("produtos/lista", { lista: results });
+                },
+                json: () => {
+                    response.json(results);
+                }
+            });
+
         });
 
         //fechando a conexão
         connection.end();
         //renderizar arquivos com o ejs, o ejs busca as views na pasta views.
-       // response.render("produtos/lista");
+        // response.render("produtos/lista");
     });
 
     //responde o formulario de produtos
-    app.get("/produtos/form", (resquest, response)=>{
+    app.get("/produtos/form", (resquest, response) => {
         response.render('produtos/form');
     })
 
-    app.post("/produtos/salva", (request, response)=>{
+    app.post("/produtos", (request, response) => {
         //capturando valor enviado
         var produto = request.body;
         console.log(produto);
-        
-        // var connection = new app.infra.ConnectionFactory().createConnection();
-        // var produtoDao = new app.dao.ProdutoDao(connection);
-        // produtoDao.salva(produto, (err, result) => {
 
-        // })
+        var connection = new app.infra.ConnectionFactory().createConnection();
+        var produtoDao = new app.dao.ProdutoDao(connection);
+        produtoDao.salva(produto, (err, result) => {
+            response.redirect('/produtos')
+        });
     })
 }
